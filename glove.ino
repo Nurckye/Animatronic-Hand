e9ec2c38b336bf6dc1bbf235b043826afb254a7d
Nurckye <42891792+Nurckye@users.noreply.github.com>
2019-02-16 09:19:53 -0800

    Add files via upload

---
+++ glove.ino
@@ -0,0 +1,50 @@
+void setup(){
+  Serial.begin(9600);
+}
+void loop() {
+  int mare = analogRead(0);
+  int arat = analogRead(1);
+  int mijl = analogRead(2);
+  int inel = analogRead(3);
+  int mic = analogRead(4);
+
+  int marep=map(mare,160,250,10,170);
+  int aratp=map(arat,115,260,10,170);
+  int mijlp=map(mijl,115,270,10,170);
+  int inelp=map(inel,150,280,10,170);
+  int micp=map(mic,150,290,10,170);
+
+  int maret=map(mare,160,250,0,49);
+  int aratt=map(arat,115,260,50,99);
+  int mijlt=map(mijl,115,270,100,149);
+  int inelt=map(inel,150,280,150,199);
+  int mict=map(mic,150,290,199,249);
+
+  if(maret>0)
+    Serial.write(maret);
+  
+  Serial.write(aratt);
+  
+  Serial.write(mijlt);
+  
+  Serial.write(inelt);
+  
+  Serial.write(mict);
+
+
+  delay (10);
+  /*if (mare > 0 && arat>0 && mijl>0 && inel>0 && mic>0) {
+    Serial.print("Nivel Indoire : ");
+    Serial.print(maret);
+    Serial.print(' ');
+    Serial.print(aratt);
+    Serial.print(' ');
+    Serial.print(mijlt);
+    Serial.print(' ');
+    Serial.print(inelt);
+    Serial.print(' ');
+    Serial.print(mict);
+    Serial.println(' ');
+    delay(1000);
+  }*/
+}
---
+++ hand.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>
+
+Servo mic;
+Servo inel;
+Servo mijl;
+Servo arat;
+Servo mare;
+int data=0;
+
+void setup() {
+
+  mic.attach(11);
+  inel.attach(10);
+  mijl.attach(9);
+  arat.attach(6);
+  mare.attach(5);
+  Serial.begin(9600);
+}
+
+
+int deget=0; //1 mare b , 2arat, 3 mijl, 4 inel, 5 mic
+int pos, pos_intermediar;
+void loop() {  
+ if(Serial.available() > 0){    
+   data = Serial.read();   
+  
+   deget= data/50+1;
+   pos_intermediar=data%50;
+   pos=map(pos_intermediar,0,49,10,170);
+   if(deget==1)
+      mare.write(pos);
+   if(deget==2)
+      arat.write(pos);
+   if(deget==3)
+      mijl.write(pos);
+   if(deget==4)
+      inel.write(170-pos);
+   if(deget==5)
+      mic.write(170-pos);
+   Serial.println(data);
+  // Serial.print(' ');
+  // Serial.print(deget);
+  //Serial.print(' ');
+   //Serial.println(pos);
+  //Serial.println(8);
+ }
+}
